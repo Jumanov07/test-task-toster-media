385226d46238556cd28614021599aef134e76bb5
Jumanov Alisher <zumanovaliser814@gmail.com>
2024-02-07 20:30:14 +0600

    finished

---
+++ src/script.js
@@ -0,0 +1,23 @@
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+const close = document.getElementById("close");
+
+burger.addEventListener("click", () => {
+  menu.className = "menu work";
+});
+
+close.addEventListener("click", () => {
+  menu.className = "menu";
+});
+
+const button = document.getElementById("buy");
+const card = document.getElementById("card");
+const closeCard = document.getElementById("close-card");
+
+button.addEventListener("click", () => {
+  card.className = "card work";
+});
+
+closeCard.addEventListener("click", () => {
+  card.className = "card";
+});
